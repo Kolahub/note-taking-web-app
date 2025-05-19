3959dcb673ee633f0948f71f379ffdef2faa8326
kolahub <ayofefaheez@gmail.com>
2025-05-20 00:14:19 +0100

    redundancy

--- src/components/notes/NoteBars.jsx
+++ src/components/notes/NoteBars.jsx
@@ -193,12 +193,8 @@ function NoteBars() {
     <div
       className="p-4 sm:px-8 sm:py-4 lg:px-4 lg:py-5 border-r-2 dark:border-gray-800 flex flex-col w-full relative"
       style={{
-        height: settingsActive
-          ? '100vh'
-          : window.innerWidth < 1024
-          ? 'calc(100vh - 200px)'
-          : 'calc(100vh - 85px)',
-        overflow: settingsActive ? 'auto' : 'hidden',
+        height: window.innerWidth < 1024 ? 'calc(100vh - 200px)' : 'calc(100vh - 85px)',
+        overflow: 'auto',
       }}
     >
       {settingsActive ? (
